eff34e07a2d1a46324c6725623dfe84af706fade
Nikhil Ahluwalia <nik@Nikhils-iMac.local>
2026-09-10 23:16:06 -0400

    Deploy orgora-web-preview 929a9bb (Backup-cancel keeps Backup off).

--- orgora-web-preview/flutter_bootstrap.js
+++ orgora-web-preview/flutter_bootstrap.js
@@ -33,10 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06cd","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06cd","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2897973245" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "542395990" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
