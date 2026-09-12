30a246f50eb57cdbf20f9cc01051bf9eeffbd4e8
Nikhil Ahluwalia <nik@Nikhils-iMac.local>
2026-09-12 12:19:04 -0400

    Redeploy orgora-web-preview b84de7a (PR #27 cloud-from-Home).

--- orgora-web-preview/flutter_bootstrap.js
+++ orgora-web-preview/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4197419764" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2301283895" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
